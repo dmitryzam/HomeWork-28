947621aa82c8f3447162cac5c3eb872c5e716705
Notebook <Notebook@DESKTOP-SGQ7DSR>
2024-01-15 00:06:57 +0500

    Add project files.

---
+++ HomeWork-28/Tree.cpp
@@ -0,0 +1,17 @@
+﻿#include "Tree.hpp"
+
+void print_tree(const Tree A) {
+	std::cout << "Наименование фрукта - " << A.fruit << '\n';
+	std::cout << "Количество плодов на дереве - " << A.fruits_number << " шт.\n";
+	std::cout << "Стоимость одного фрукта - " << A.fruits << " руб.\n";
+};
+int full_price(const Tree A){
+	return A.fruits_number*A.fruits;
+};
+int collect(Tree& A, int num) {
+	if (A.fruits_number < num)
+		return -1;
+	else
+		A.fruits_number -= num;
+	return A.fruits_number * A.fruits;
+};
---
+++ HomeWork-28/Tree.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include<iostream>
+#include<string>
+
+struct Tree {
+	std::string fruit;		// наименование фрукта
+	int fruits_number;		// кол-во плодов на дереве
+	int fruits;				// цена за один фрукт
+};
+// Функция по выводу объекта в консоль:
+void print_tree(const Tree A);
+// Функция по расчету стоимости всех фруктов на дереве:
+int full_price(const Tree A);
+// Функция по расчету стоимости всех оставшихся фруктов на дереве с учетом сбора:
+int collect(Tree& A, int num);
